9c628eafc16da7fd802376f63f8d77c91069d36e
102AMIT <kingston.amit9830@gmail.com>
2023-07-07 00:26:36 +0530

    Resume section completed

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import { motion } from "framer-motion";
 import { FaUser, FaEnvelope, FaGoogle } from "react-icons/fa";
 import { IoIosPaper } from "react-icons/io";
 import { MdWork, MdOutlineClose } from "react-icons/md";
@@ -6,8 +7,17 @@ import { BsTelephonePlusFill } from "react-icons/bs";
 import Left from "./components/Home/Left";
 import About from "./components/About/About";
 import Resume from "./components/Resume/Resume";
+import Project from "./components/Projects/Projects";
+import Contact from "./components/Contact/Contact";
+import Blog from "./components/Blog/Blog";
 
 const Home = () => {
+  const [about, setAbout] = useState(true);
+  const [resume, setResume] = useState(false);
+  const [contact, setContact] = useState(false);
+  const [project, setProject] = useState(false);
+  const [sidenav, setSidenav] = useState(false);
+  const [blog, setBlog] = useState(false);
   return (
     <div className="w-[85%] h-[85%] bg-transparent text-white z-50 flex items-start justify-between">
       <div className="w-16 h-96 bg-transparent flex flex-col gap-2">
@@ -21,37 +31,91 @@ const Home = () => {
         </div>
         {/* other icons */}
         <div className="w-full h-80 bg-bodyColor rounded-3xl py-6 flex flex-col items-center justify-between">
-          <span className="w-full h-6 text-textColor items-center flex justify-center text-xl hover:text-designColor duration-300 cursor-pointer relative group">
+          <span
+            onClick={() =>
+              setAbout(true) &
+              setResume(false) &
+              setContact(false) &
+              setProject(false) &
+              setBlog(false)
+            }
+            className="w-full h-6 text-textColor items-center flex justify-center text-xl hover:text-designColor duration-300 cursor-pointer relative group"
+          >
             <FaUser />
             <span className="absolute text-black font-medium text-xs uppercase bg-designColor px-4 py-[1px] rounded-xl left-0 translate-x-8 group-hover:translate-x-12 transition-all duration-300 z-20 opacity-0 group-hover:opacity-100">
               About
             </span>
           </span>
-          <span className="w-full h-6 text-textColor items-center flex justify-center text-xl hover:text-designColor duration-300 cursor-pointer relative group">
+          <span
+            onClick={() =>
+              setAbout(false) &
+              setResume(true) &
+              setContact(false) &
+              setProject(false) &
+              setBlog(false)
+            }
+            className="w-full h-6 text-textColor items-center flex justify-center text-xl hover:text-designColor duration-300 cursor-pointer relative group"
+          >
             <IoIosPaper />
             <span className="absolute text-black font-medium text-xs uppercase bg-designColor px-4 py-[1px] rounded-xl left-0 translate-x-8 group-hover:translate-x-12 transition-all duration-300 z-20 opacity-0 group-hover:opacity-100">
               Resume
             </span>
           </span>
-          <span className="w-full h-6 text-textColor items-center flex justify-center text-xl hover:text-designColor duration-300 cursor-pointer relative group">
+          <span
+            onClick={() =>
+              setAbout(false) &
+              setResume(false) &
+              setContact(false) &
+              setProject(true) &
+              setBlog(false)
+            }
+            className="w-full h-6 text-textColor items-center flex justify-center text-xl hover:text-designColor duration-300 cursor-pointer relative group"
+          >
             <MdWork />
             <span className="absolute text-black font-medium text-xs uppercase bg-designColor px-4 py-[1px] rounded-xl left-0 translate-x-8 group-hover:translate-x-12 transition-all duration-300 z-20 opacity-0 group-hover:opacity-100">
               Projects
             </span>
           </span>
-          <span className="w-full h-6 text-textColor items-center flex justify-center text-xl hover:text-designColor duration-300 cursor-pointer relative group">
+          <span
+            onClick={() =>
+              setAbout(false) &
+              setResume(false) &
+              setContact(false) &
+              setProject(false) &
+              setBlog(true)
+            }
+            className="w-full h-6 text-textColor items-center flex justify-center text-xl hover:text-designColor duration-300 cursor-pointer relative group"
+          >
             <FaGoogle />
             <span className="absolute text-black font-medium text-xs uppercase bg-designColor px-4 py-[1px] rounded-xl left-0 translate-x-8 group-hover:translate-x-12 transition-all duration-300 z-20 opacity-0 group-hover:opacity-100">
               Blog
             </span>
           </span>
-          <span className="w-full h-6 text-textColor items-center flex justify-center text-xl hover:text-designColor duration-300 cursor-pointer relative group">
+          <span
+            onClick={() =>
+              setAbout(false) &
+              setResume(false) &
+              setContact(true) &
+              setProject(false) &
+              setBlog(false)
+            }
+            className="w-full h-6 text-textColor items-center flex justify-center text-xl hover:text-designColor duration-300 cursor-pointer relative group"
+          >
             <FaEnvelope />
             <span className="absolute text-black font-medium text-xs uppercase bg-designColor px-4 py-[1px] rounded-xl left-0 translate-x-8 group-hover:translate-x-12 transition-all duration-300 z-20 opacity-0 group-hover:opacity-100">
               Contact
             </span>
           </span>
-          <span className="w-full h-6 text-textColor items-center flex justify-center text-xl hover:text-designColor duration-300 cursor-pointer relative group">
+          <span
+            onClick={() =>
+              setAbout(false) &
+              setResume(false) &
+              setContact(true) &
+              setProject(false) &
+              setBlog(false)
+            }
+            className="w-full h-6 text-textColor items-center flex justify-center text-xl hover:text-designColor duration-300 cursor-pointer relative group"
+          >
             <BsTelephonePlusFill />
             <span className="absolute text-black font-medium text-xs uppercase bg-designColor px-4 py-[1px] rounded-xl left-0 translate-x-8 group-hover:translate-x-12 transition-all duration-300 z-20 opacity-0 group-hover:opacity-100">
               Call
@@ -62,12 +126,56 @@ const Home = () => {
       <div className="w-[94%] h-full bg-transparent flex items-center">
         {/* left part start */}
         <Left />
-      {/* right part start */}
-      <div className="w-8/12 h-[95%] bg-bodyColor ">
-        <div className="w-full h-[96%] overflow-y-scroll scrollbar-thin scrollbar-thumb-[#646464]">
-          {/* <About /> */}
-          <Resume />
-        </div>
+        {/* right part start */}
+        <div className="w-8/12 h-[95%] bg-bodyColor ">
+          <div className="w-full h-[96%] overflow-y-scroll scrollbar-thin scrollbar-thumb-[#646464]">
+          {about && (
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 0.5 }}
+            >
+              <About />
+            </motion.div>
+          )}
+          {resume && (
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 0.5 }}
+            >
+              <Resume />
+            </motion.div>
+          )}
+          {project && (
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 0.5 }}
+            >
+              <Project />
+            </motion.div>
+          )}
+          {blog && (
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 0.5 }}
+            >
+              <Blog />
+            </motion.div>
+          )}
+          {contact && (
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 0.5 }}
+            >
+              <Contact />
+            </motion.div>
+          )}
+
+          </div>
         </div>
       </div>
     </div>
